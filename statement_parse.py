66bc2d15f8538be27e26071f8b58be3e34d34373
nizalovnd <ndn1@kent.ac.uk>
2026-03-03 16:52:25 +0000

    Implementing the rename function to identiy and rename statements from different banks

--- statement_parse.py
+++ statement_parse.py
@@ -1,5 +1,21 @@
 import pdfplumber
+import json
+import re
 from pathlib import Path
+
+base_dir = Path.cwd()
+
+
+settings_aqua = {
+    "vertical_strategy": "explicit",
+    "horizontal_strategy": "lines",
+    "explicit_vertical_lines": [28, 95, 500, 568],  # Date | Description | Amount
+    "snap_tolerance": 5,
+    "intersection_y_tolerance": 10,
+    "explicit_horizontal_lines": [795]
+}
+
+
 
 class Transaction():
     def __init__(self, date,  account, amount):
@@ -18,15 +34,56 @@ class Expense(Transaction):
         self.category = category
         self.card_type = card_type
 
+def file_rename():
+    dir_path = Path(base_dir/"statements")
+    for file in dir_path.iterdir():
+        if not file.is_file():
+            continue
+        elif re.search(r"\Aaccount",file.name):
+            #revolut
+            new_name = dir_path / "revolut.pdf"
+            file.rename(new_name)
+        elif re.search(r"\d{4}-\d{2}-\d{2}",file.name):
+            #aqua
+            new_name = dir_path / "aqua.pdf"
+            file.rename(new_name)
+        elif re.search(r"\d{4}_\d{2}_\d{2}",file.name):
+            #capital_one
+            new_name = dir_path / "capital_one.pdf"
+            file.rename(new_name)
+        
+        elif re.search(r"\d{4}\sStatement.pdf\Z",file.name):
+            #nationwide
+            new_name = dir_path / "nationwide.pdf"
+            file.rename(new_name)
+        elif re.search(r"\bStatement",file.name):
+            #natwest
+            new_name = dir_path / "natwest.pdf"
+            file.rename(new_name)
+        else:
+            print("Unknown Statement")
+            continue
+
+
 
 def pdf_to_json(file_path):
     #new json file for this file
+    intermediate_list = []
+    json_filename = "db1.json"
     with pdfplumber.open(file_path) as pdf:
-        for i in range(len(pdf.pages)):
-            page = pdf.pages[i]
-            table = page.extract_table()
+        for i, page in enumerate(pdf.pages):
+            if i == 0:
+                continue
+            page_text = page.extract_text()
+            if "Your account in detail" not in page_text or "Your interest rates" in page_text:
+                break
+            table = page.extract_table(table_settings=settings_aqua)
+            intermediate_list.append(table)
 
-
-
+    try:
+        with open(json_filename, "w") as file:
+            json.dump(intermediate_list, file, )
+    except IOError:
+        print("Error writing json file")
 
 
