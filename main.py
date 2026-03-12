0ccef669b8e3c4e1d6f546c5de240ce6e2600af2
nizalovnd <ndn1@kent.ac.uk>
2026-03-12 16:20:59 +0000

    adding multiprocessing using a pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 
+from multiprocessing import Pool
 from pathlib import Path
 
 from statement_parse import *
@@ -10,11 +11,11 @@ statement_dir = Path(base_dir / "statements")
 output_dir = Path(base_dir / "output")
 
 # for iteration through statements
-statement_settings = [("aqua", "aqua.pdf", settings_aqua, AquaFilter(), AquaTableFilter(), AquaTransaction()),
-                      ("capital" "capital_one.pdf", settings_capital, CapitalFilter(), CapitalTableFilter(), CapitalTransaction()),
-                       ("nationwide", "nationwide.pdf", settings_nationwide, NationwideFilter(), NationwideTableFilter(), NationwideTransaction()),
-                       ("natwest", "natwest.pdf", settings_natwest, NatwestFilter(), NatwestTableFilter(), NatwestTransaction()),
-                       ("revolut", "revolut.pdf", settings_revolut, RevolutFilter(), RevolutTableFilter(), RevolutTransaction())]
+statement_settings = [( "aqua.pdf", settings_aqua, AquaFilter(), AquaTableFilter(), AquaTransaction()),
+                      ("capital_one.pdf", settings_capital, CapitalFilter(), CapitalTableFilter(), CapitalTransaction()),
+                       ("nationwide.pdf", settings_nationwide, NationwideFilter(), NationwideTableFilter(), NationwideTransaction()),
+                       ("natwest.pdf", settings_natwest, NatwestFilter(), NatwestTableFilter(), NatwestTransaction()),
+                       ("revolut.pdf", settings_revolut, RevolutFilter(), RevolutTableFilter(), RevolutTransaction())]
 
 
 def file_rename():
@@ -48,15 +49,13 @@ def file_rename():
             continue
 
 
-def main():
-    expenses = []
-    incomes = []
-    file_rename()
-
-    for bank, pdf_statement, pdf_settings, page_filter, table_filter, object_creator in statement_settings:
-        pdf_path = statement_dir / pdf_statement
-        if not pdf_path.exists():
-            continue
+def process_statements(pdf_path, pdf_settings, page_filter, table_filter, object_creator ):
+    
+    
+        expenses = []
+        incomes = []
+    
+        
 
         pdf = PDFProcessor(page_filter, pdf_settings)
         table = TableProcessor(table_filter)
@@ -78,13 +77,33 @@ def main():
                     incomes.append(transaction_dict)
                 else:
                     pass
+        return incomes, expenses
         #TODO convert to dict form for json serialization
-
-        with open("expneses.json", "w") as file:
-            json.dump(expenses, file)
-
-        with open("incomes.json", "w") as file:
-            json.dump(incomes, file)
 
         
 
+        
+def main():
+    
+    
+    file_rename()
+
+    with Pool(processes=5) as pool:
+
+        for pdf_statement, pdf_settings, page_filter, table_filter, object_creator in statement_settings:
+            pdf_path = statement_dir / pdf_statement
+            if not pdf_path.exists():
+                continue
+        results = pool.starmap(process_statements, statement_settings)
+            
+       # expenses =  [expenses for _, expenses in results]
+        #final_expenses = [*e for e in expenses]
+        expenses = [e for _, expenses in results for e in expenses]
+        incomes = [i for incomes, _ in results for i in incomes]
+
+
+    with open("expneses.json", "w") as file:
+            json.dump(expenses, file)
+
+    with open("incomes.json", "w") as file:
+        json.dump(incomes, file)
