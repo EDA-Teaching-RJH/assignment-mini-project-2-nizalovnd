28fd93f62f746d2d698a9ff7f8af283474d2be7a
nizalovnd <ndn1@kent.ac.uk>
2026-03-18 13:28:00 +0000

    start of implementing testing

---
+++ test_main.py
@@ -0,0 +1,14 @@
+from main import file_rename, process_statements, bubble_sort, main
+
+def test_file_rename():
+    pass
+
+def test_process_statements():
+    pass
+
+def test_bubble_sort():
+    pass
+
+def test_main():
+    pass
+
