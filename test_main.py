b3b367eb9b7ee7f004214aa540484a243ffd6626
nizalovnd <ndn1@kent.ac.uk>
2026-03-21 15:30:52 +0000

    testing v3 test_file_rename()

--- test_main.py
+++ test_main.py
@@ -1,19 +1,29 @@
 import pytest
+import main as main_script
 
 from main import file_rename, process_statements, bubble_sort, main
 
 from unittest.mock import patch
 
-def test_file_rename():
-    fake_file = fake_path / file
+def rename_helper_func(fake_path, filename):
+    fake_file = fake_path / filename
     fake_file.touch()
-    with patch.object(main, "statemet_dir", fake_path):
-        main.file_rename()
-    assert ("aqua.pdf").exists()
-    assert ("revolut.pdf").exists()
-    assert ("capital_one.pdf").exists()
-    assert ("nationwide.pdf").exists()
-    assert ("natwest.pdf").exists()
+    with patch.object(main_script, "statement_dir", fake_path):
+        main_script.file_rename()
+
+
+def test_file_rename(tmp_path):
+    rename_helper_func(tmp_path, "account_statement.pdf")
+    assert (tmp_path/ "revolut.pdf").exists()
+    
+    rename_helper_func(tmp_path, "2025-12-12statement.pdf")
+    assert (tmp_path / "aqua.pdf").exists()
+    rename_helper_func(tmp_path, "2025_12_12statement.pdf")
+    assert (tmp_path / "capital_one.pdf").exists()
+    rename_helper_func(tmp_path, "Jan 2026 Statement.pdf")
+    assert (tmp_path / "nationwide.pdf").exists()
+    rename_helper_func(tmp_path, "Statement_23452345_123feb.pdf")
+    assert (tmp_path / "natwest.pdf").exists()
     #TODO find out how to mock statement subdir and mock the pdf files to rename
 
 def test_process_statements():
