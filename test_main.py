3d205f121dcd8c18136b879fddee133e53bfa904
nizalovnd <ndn1@kent.ac.uk>
2026-03-21 15:47:47 +0000

    testing v4 test_main and test_process_statement layout

--- test_main.py
+++ test_main.py
@@ -24,10 +24,18 @@ def test_file_rename(tmp_path):
     assert (tmp_path / "nationwide.pdf").exists()
     rename_helper_func(tmp_path, "Statement_23452345_123feb.pdf")
     assert (tmp_path / "natwest.pdf").exists()
-    #TODO find out how to mock statement subdir and mock the pdf files to rename
+    rename_helper_func(tmp_path, "unknown_file.pdf")
+    assert (tmp_path / "unknown_file.pdf").exists()
+   
 
 def test_process_statements():
-    pass
+    """
+    assert no pdf -> empty
+    assert incomes added
+    assert expenses added
+    assert pages skipped???
+    assert None rows are skipped?
+    """
 
 def test_bubble_sort():
     assert bubble_sort([]) == []
@@ -36,5 +44,13 @@ def test_bubble_sort():
     assert bubble_sort([{"date": "2026-01-09", "name": "Millevoglie Venezia ITA\nCurrency conversion rate 4,00 EUR @ 1.1494", "account": "Aqua", "amount": -3.48, "category": "expense", "card_type": "credit"}, {"date": "2026-01-10", "name": "Foreign Exchange Conversion Charge", "account": "Aqua", "amount": -1.0, "category": "expense", "card_type": "credit"}, {"date": "2026-01-08", "name": "Foreign Exchange Conversion Charge", "account": "Aqua", "amount": -0.35, "category": "expense", "card_type": "credit"}]) == [{"date": "2026-01-08", "name": "Foreign Exchange Conversion Charge", "account": "Aqua", "amount": -0.35, "category": "expense", "card_type": "credit"},{"date": "2026-01-09", "name": "Millevoglie Venezia ITA\nCurrency conversion rate 4,00 EUR @ 1.1494", "account": "Aqua", "amount": -3.48, "category": "expense", "card_type": "credit"}, {"date": "2026-01-10", "name": "Foreign Exchange Conversion Charge", "account": "Aqua", "amount": -1.0, "category": "expense", "card_type": "credit"} ]
    
 def test_main():
-    pass
+    """
+    mock the multiprocessing (pool)
+    mock json.dump
+    mock process_statements
+    mock file_rename
+    
+    assert doesn't crash????
+
+    """
 
